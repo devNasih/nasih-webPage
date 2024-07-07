e5a910b19a922150e7860a4bc4858b3ff29ee4ec
devNasih <nasihameen902@gmail.com>
2024-07-07 21:29:19 +0530

    Issues in assets

--- src/home/first_page.jsx
+++ src/home/first_page.jsx
@@ -7,7 +7,7 @@ export default function FirstPage() {
         <div className="col-xl-6 col-lg-6 col-md-12 user-Image">
           <div className="Image-0">
             <img
-              src="src/assets/DSC00052.JPG"
+              src="/assets/DSC00052.JPG"
               alt="my-image"
               width={400}
               height={400}
@@ -20,18 +20,14 @@ export default function FirstPage() {
             <h6>Hello, I&apos;m</h6>
           </div>
           <div className="User-name">
-          <h3>Nasih Ameen K</h3>
+            <h3>Nasih Ameen K</h3>
           </div>
           <div className="user-designation">
-          <h4>Full Stack Developer</h4>
+            <h4>Full Stack Developer</h4>
           </div>
           <div className="user-buttons">
-            <button className="resume-btn">
-              Resume
-            </button>
-            <button className="contact-btn">
-              Contact Info
-            </button>
+            <button className="resume-btn">Resume</button>
+            <button className="contact-btn">Contact Info</button>
           </div>
         </div>
       </div>
